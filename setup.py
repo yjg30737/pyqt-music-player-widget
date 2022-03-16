793cae2832e56f74f3a034628e15b46bccec8021
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-16 19:07:16 +0900

    Fix typo

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
         'PyQt5>=5.8',
         'mutagen>=1.45.1',
         'pyqt-music-slider @ git+https://git@github.com/yjg30737/pyqt-music-slider.git@main',
-        'pyqt-svg-icon-pushbutton @ git+https://git@github.com/yjg30737/pyqt-svg-icon-pushbutton.git@main''
+        'pyqt-svg-icon-pushbutton @ git+https://git@github.com/yjg30737/pyqt-svg-icon-pushbutton.git@main'
     ]
 )
