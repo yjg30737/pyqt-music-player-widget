410e4cbae79392ac1f7176c7006320ba3f682382
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-17 13:13:52 +0900

    Update version (0.3.0 -> 0.3.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-music-player-widget',
-    version='0.3.0',
+    version='0.3.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
