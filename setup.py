9b9c68fcd47c6a77fa174a614c3da61ced36ad03
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-21 08:11:17 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-music-player-widget',
-    version='0.3.1',
+    version='0.3.2',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
